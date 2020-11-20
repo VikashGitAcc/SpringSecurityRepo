c9bf865610c5b8bb61ea67edb051a759e6f236ac
Vikash Admin <vikash.srivastava@velocis.in>
2020-11-20 12:45:25 +0530

    Demo - to test the Filter class that is given Spring Security bydefault

---
+++ Demo_ToTestSecurityFilterClass/src/main/java/com/example/DemoToTestSecurityFilterClassApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoToTestSecurityFilterClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoToTestSecurityFilterClassApplication.class, args);
+	}
+
+}
