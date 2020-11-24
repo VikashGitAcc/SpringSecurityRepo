6741215abe7eb0e77494a1bbcb16c7f71a9a1675
Vikash Admin <vikash.srivastava@velocis.in>
2020-11-24 15:17:11 +0530

    Demo - Spring Security Demo Class for Custome Page

---
+++ Demo_ToTestSecurity_CustomeLogin_InBootApp/src/main/java/com/example/CustomeController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomeController {
+
+	@RequestMapping(value = "/login")
+	public String logFun() {
+		System.out.println("Welcome Login Page");
+		//return "Welcome Login Page";
+		
+		return "index";
+	}
+}
